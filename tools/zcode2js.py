05b51697945018779aad151338a4d94873449948
Andrew Plotkin <erkyrath@eblong.com>
2010-06-24 00:59:53 -0400

    Added the zcode2js.py script out of Parchment.

---
+++ tools/zcode2js.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+    zcode2js.py
+
+    This utility converts a z-code (or glulx) story file into a Javascript 
+    file for use by Parchment or Quixe.
+
+    Usage is as follows:
+
+      python zcode2js.py <game-file>
+
+    The result is printed to stdout, so you'll probably want to pipe
+    the output to a file, e.g.:
+
+      python zcode2js.py mystory.z5 > mystory.z5.js
+"""
+
+import os
+import sys
+import base64
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print __import__("__main__").__doc__
+        sys.exit(-1)
+    contents = open(sys.argv[1], "rb").read()
+    print "processBase64Zcode('%s');" % (
+        base64.b64encode(contents)
+        )
