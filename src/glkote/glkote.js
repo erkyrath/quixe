4f80c39a5b711eeb71fb34b3d24e58af2ef8ded6
Andrew Plotkin <erkyrath@eblong.com>
2010-07-04 15:00:56 -0400

    Pull changes from glkote.

--- src/glkote/glkote.js
+++ src/glkote/glkote.js
@@ -1437,7 +1437,7 @@ function build_evhan_hyperlink(winid, linkval) {
 /* End of GlkOte namespace function. Return the object which will
    become the GlkOte global. */
 return {
-  version:  '1.1.0',
+  version:  '1.1.1',
   init:     glkote_init, 
   update:   glkote_update,
   extevent: glkote_extevent,
