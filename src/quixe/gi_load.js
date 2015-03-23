7ddcb37d69b8a7ae025c68579e8a06029f790219
Andrew Plotkin <erkyrath@eblong.com>
2015-03-23 17:49:54 -0400

    Tighten up the encode_base64 polyfill.

--- src/quixe/gi_load.js
+++ src/quixe/gi_load.js
@@ -720,34 +720,21 @@ else {
     encode_base64 = function(arr) {
         var coder = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
         var res = [];
+        var byte0, byte1, byte2;
         for (var ix=0; ix<arr.length; ix += 3) {
-            var byte0 = arr[ix];
-            var byte1 = arr[ix+1];
-            var byte2 = arr[ix+2];
-            var count = 3;
-            if (byte2 === undefined) {
-                byte2 = 0;
-                count = 2;
-            }
-            if (byte1 === undefined) {
-                byte1 = 0;
-                count = 1;
-            }
+            byte0 = arr[ix];
+            byte1 = arr[ix+1];
+            byte2 = arr[ix+2];
             res.push(coder.charAt((byte0 >> 2) & 0x3F));
             res.push(coder.charAt(((byte0 << 4) & 0x30) | ((byte1 >> 4) & 0x0F)));
-            if (count == 1) {
-                res.push('=');
-                res.push('=');
-            }
-            else {
-                res.push(coder.charAt(((byte1 << 2) & 0x3C) | ((byte2 >> 6) & 0x03)));
-                if (count == 2) {
-                    res.push('=');
-                }
-                else {
-                    res.push(coder.charAt((byte2) & 0x3F));
-                }
-            }
+            res.push(coder.charAt(((byte1 << 2) & 0x3C) | ((byte2 >> 6) & 0x03)));
+            res.push(coder.charAt((byte2) & 0x3F));
+        }
+        if (byte1 === undefined && res.length >= 2) {
+            res[res.length-2] = '=';
+        }
+        if (byte2 === undefined && res.length >= 1) {
+            res[res.length-1] = '=';
         }
         return res.join('');
     }
